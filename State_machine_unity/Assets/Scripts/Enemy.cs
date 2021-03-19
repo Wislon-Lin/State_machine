e3f39138e8bd91b37cceb23acef8bbce3d4185d1
WislonLin <p96091123@gs.ncku.edu.tw>
2021-03-19 18:28:01 +0800

    Version_01

---
+++ State_machine_unity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum Status{ idel,walk  }
+    public Status status;
+
+    public enum Face { Right,Left }
+    public Face face;
+
+    public float speed;
+
+    private Transform myTransform;
+
+    private Transform playerTransform;
+
+    private SpriteRenderer spr;
+
+    void Start ()
+    {
+        status = Status.idel;
+
+        spr = this.transform.GetComponent<SpriteRenderer>();
+
+        if (spr.flipX)
+        {
+            face = Face.Left;
+        }
+        else
+        {
+            face = Face.Right;
+        }
+
+        myTransform = this.transform;
+
+        if(GameObject.Find("Player") != null)
+        {
+           playerTransform = GameObject.Find("Player").transform;
+        }
+       
+    }
+	
+	void Update ()
+    {
+        if (playerTransform) 
+        {
+            if (Mathf.Abs(myTransform.position.x - playerTransform.position.x) > 2)/*當敵人根Player位置大於2時 變成idel*/
+            {
+                status = Status.idel;
+                SetEnemyState(status);
+            }
+            else if (Mathf.Abs(myTransform.position.x - playerTransform.position.x) < 2)/*當敵人根Player位置小於2時 變成walk*/
+            {
+                status = Status.walk;
+                SetEnemyState(status);
+              
+            }
+
+          
+        }
+
+
+
+
+
+    }
+
+    public void SetEnemyState(Status EnemyState)
+    {
+        status = EnemyState;
+        switch(status)
+        {
+            case Status.idel:
+                Debug.Log("IDEL狀態");
+                break;
+
+            case Status.walk: /*敵人走路狀態*/
+                Debug.Log("走路狀態");
+                if (playerTransform)
+                {
+                    if (myTransform.position.x >= playerTransform.position.x) /*當敵人的位置在玩家右邊(敵人要看左邊)*/
+                    {
+                        spr.flipX = true;
+                        face = Face.Left;
+                        SetEnemyFace(face);
+                    }
+                    else                                                      /*當敵人的位置在玩家左邊(敵人要看右邊)*/
+                    {
+                        spr.flipX = false;
+                        face = Face.Right;
+                        SetEnemyFace(face);
+                    }
+                }
+                break;
+        }
+    }
+
+    public void SetEnemyFace(Face newFaceState) 
+    {
+        face = newFaceState;
+        switch (face)
+        {
+            case Face.Right: /*敵人向右走路*/
+                myTransform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+                break;
+
+            case Face.Left:  /*敵人向左走路*/
+                myTransform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+                break;
+        }
+
+    }
+
+}
---
+++ State_machine_unity/Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerState currentState; /*運用這個currentState*/
+    public enum PlayerState /*列舉 列出*/
+    {
+        Idel,
+        WalkLeft,
+        WalkRight
+    }
+
+    private SpriteRenderer PlayerSprite; /*抓取PlayerSprite圖來控制Flip(翻轉)*/
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        currentState = PlayerState.Idel;
+        PlayerSprite = this.transform.GetComponent<SpriteRenderer>();
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void Update() /*吃效能的地方*/ 
+    {
+       switch (currentState)     /*一直持續判斷currentState的狀態去做切換*/
+        {
+            case PlayerState.Idel:
+                SetState(currentState);     
+                break;
+
+            case PlayerState.WalkLeft:
+                SetState(currentState);
+                break;
+
+            case PlayerState.WalkRight:
+                SetState(currentState);
+                break;
+        }
+        /*-------------------------------------------------------*/
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentState = PlayerState.WalkRight;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            currentState = PlayerState.Idel;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentState = PlayerState.WalkLeft;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            currentState = PlayerState.Idel;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow)&&isJump==false)
+        {
+            rb.AddForce(transform.up *400);
+            isJump = true;
+        }
+
+    }
+
+    public void SetState(PlayerState newState)  
+    {
+        currentState = newState;
+        switch (currentState)
+        {
+            case PlayerState.Idel:
+                Debug.Log("Idel");
+                break;
+            case PlayerState.WalkLeft:
+                Debug.Log("WalkingLeft");
+                this.transform.position -= new Vector3(2 * Time.deltaTime, 0, 0);
+                PlayerSprite.flipX = true;
+                break;
+            case PlayerState.WalkRight:
+                this.transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+                Debug.Log("WalkingRight");
+                PlayerSprite.flipX = false;
+                break;
+        }
+    }
+
+
+
+    private bool isJump =false;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag=="Floor")
+        {
+            isJump = false;
+        }
+    }
+
+
+
+}
